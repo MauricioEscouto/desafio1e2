97b72385fe124e707c4fe5034233159d6336eb98
Mauricio Escouto Melo <103608018+MauricioEscouto@users.noreply.github.com>
2023-07-25 20:09:43 -0300

    Add files via upload

---
+++ Desafio1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+public class Desafio1
+{
+    public static void Main()
+    {
+        Node root = null;
+        root = Inserir(root, 3);
+        root = Inserir(root, 2);
+        root = Inserir(root, 4);
+        root = Inserir(root, 5);
+        root = Inserir(root, 6);
+
+        PercursoEmOrdem(root);
+    }
+
+    public class Node
+    {
+        public int valor;
+        public Node esquerda;
+        public Node direita;
+        public int altura;
+
+        public Node(int val)
+        {
+            this.valor = val;
+            this.esquerda = null;
+            this.direita = null;
+            this.altura = 0;
+        }
+    }
+
+    public static Node Inserir(Node raiz, int novoValor)
+    {
+        if (raiz == null)
+        {
+            raiz = new Node(novoValor);
+        }
+        else if (novoValor < raiz.valor)
+        {
+            raiz.esquerda = Inserir(raiz.esquerda, novoValor);
+        }
+        else
+        {
+            raiz.direita = Inserir(raiz.direita, novoValor);
+        }
+
+        AtualizarAltura(raiz);
+
+        int fatorBalanceamento = Altura(raiz.esquerda) - Altura(raiz.direita);
+
+        if (fatorBalanceamento > 1)
+        {
+            if (novoValor < raiz.esquerda.valor)
+            {
+                raiz = RotacaoDireita(raiz);
+            }
+            else
+            {
+                raiz.esquerda = RotacaoEsquerda(raiz.esquerda);
+                raiz = RotacaoDireita(raiz);
+            }
+        }
+        else if (fatorBalanceamento < -1)
+        {
+            if (novoValor > raiz.direita.valor)
+            {
+                raiz = RotacaoEsquerda(raiz);
+            }
+            else
+            {
+                raiz.direita = RotacaoDireita(raiz.direita);
+                raiz = RotacaoEsquerda(raiz);
+            }
+        }
+
+        return raiz;
+    }
+
+    public static int Altura(Node raiz)
+    {
+        if (raiz == null) return -1;
+        return raiz.altura;
+    }
+
+    public static void AtualizarAltura(Node raiz)
+    {
+        if (raiz == null) return;
+        int alturaEsquerda = Altura(raiz.esquerda);
+        int alturaDireita = Altura(raiz.direita);
+        raiz.altura = Math.Max(alturaEsquerda, alturaDireita) + 1;
+    }
+
+    public static Node RotacaoDireita(Node raiz)
+    {
+        Node novaRaiz = raiz.esquerda;
+        Node novaRaizDireita = novaRaiz.direita;
+        novaRaiz.direita = raiz;
+        raiz.esquerda = novaRaizDireita;
+        AtualizarAltura(raiz);
+        AtualizarAltura(novaRaiz);
+        return novaRaiz;
+    }
+
+    public static Node RotacaoEsquerda(Node raiz)
+    {
+        Node novaRaiz = raiz.direita;
+        Node novaRaizEsquerda = novaRaiz.esquerda;
+        novaRaiz.esquerda = raiz;
+        raiz.direita = novaRaizEsquerda;
+        AtualizarAltura(raiz);
+        AtualizarAltura(novaRaiz);
+        return novaRaiz;
+    }
+
+    public static void PercursoEmOrdem(Node raiz)
+    {
+        if (raiz != null)
+        {
+            PercursoEmOrdem(raiz.esquerda);
+            Console.Write(raiz.valor + "(BF=" + (Altura(raiz.esquerda) - Altura(raiz.direita)) + ") ");
+            PercursoEmOrdem(raiz.direita);
+        }
+    }
+}
---
+++ Desafio2.cs
@@ -0,0 +1,49 @@
+﻿public class NoListaEncadeadaSimples
+{
+    public int dado;
+    public NoListaEncadeadaSimples proximo;
+
+    public NoListaEncadeadaSimples(int dado)
+    {
+        this.dado = dado;
+        this.proximo = null;
+    }
+}
+
+public class ListaEncadeada
+{
+    public static int TemCiclo(NoListaEncadeadaSimples cabeca)
+    {
+        if (cabeca == null || cabeca.proximo == null)
+            return 0;
+
+        NoListaEncadeadaSimples lento = cabeca;
+        NoListaEncadeadaSimples rapido = cabeca.proximo;
+
+        while (rapido != null && rapido.proximo != null)
+        {
+            if (lento == rapido)
+                return 1;
+
+            lento = lento.proximo;
+            rapido = rapido.proximo.proximo;
+        }
+
+        return 0;
+    }
+}
+
+public class Desafio2
+{
+    public static void Main(string[] args)
+    {
+        NoListaEncadeadaSimples cabeca1 = new NoListaEncadeadaSimples(1);
+        Console.WriteLine(ListaEncadeada.TemCiclo(cabeca1));
+
+        NoListaEncadeadaSimples cabeca2 = new NoListaEncadeadaSimples(1);
+        cabeca2.proximo = new NoListaEncadeadaSimples(2);
+        cabeca2.proximo.proximo = new NoListaEncadeadaSimples(3);
+        cabeca2.proximo.proximo.proximo = cabeca2.proximo; 
+        Console.WriteLine(ListaEncadeada.TemCiclo(cabeca2)); 
+    }
+}
